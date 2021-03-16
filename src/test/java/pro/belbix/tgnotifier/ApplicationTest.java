cdb4c7f9135ba462238cf05c26f06f52dfaa0796
yagosupro <yagosupro@gmail.com>
2021-03-16 17:37:47 +0300

    3870-b Add GitHub actions config + sort out tests

--- src/test/java/pro/belbix/tgnotifier/ApplicationTest.java
+++ src/test/java/pro/belbix/tgnotifier/ApplicationTest.java
@@ -9,22 +9,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 import pro.belbix.tgnotifier.tg.TelegramBotService;
 import pro.belbix.tgnotifier.ws.WebSocketService;
 
+import static pro.belbix.tgnotifier.properties.ConstantsTest.REPEAT_TIME;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("test")
 public class ApplicationTest {
 
-  @Autowired
-  private WebSocketService webSocketService;
-  @Autowired
-  private TelegramBotService telegramBotService;
+    @Autowired
+    private WebSocketService webSocketService;
+    @Autowired
+    private TelegramBotService telegramBotService;
 
-  @Test
-  public void main() throws InterruptedException {
-    webSocketService.start();
-    telegramBotService.init();
-    while (true) {
-      Thread.sleep(100);
+    @Test
+    public void main() throws InterruptedException {
+        webSocketService.start();
+        telegramBotService.init();
+
+        for (int i = 0; i < REPEAT_TIME; i++) {
+            Thread.sleep(100);
+        }
     }
-  }
 }
