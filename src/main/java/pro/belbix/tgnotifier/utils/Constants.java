4d126e6d330a052b48942d24a02fb522b3a3a4ba
yagosupro <yagosupro@gmail.com>
2021-03-17 17:43:12 +0300

    5020-b Total TVL doesn't work - create Constants - create NumberHelper for similar methods - refactoring HarvestDTO

---
+++ src/main/java/pro/belbix/tgnotifier/utils/Constants.java
@@ -0,0 +1,10 @@
+package pro.belbix.tgnotifier.utils;
+
+public class Constants {
+
+  //Smiles
+  public final static String SAD_SMILE = "?\uD83D\uDE29?";
+  public final static String MONEY_BUG_SMILE = "\uD83D\uDCB0";
+  public final static String WIND_SMILE = "\uD83D\uDCA8";
+
+}
